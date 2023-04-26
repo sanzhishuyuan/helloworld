a512050e27c824950cd4e5f0d77fe64a8eac5287
sanzhishuyuan <131636493+sanzhishuyuan@users.noreply.github.com>
2023-04-26 16:53:03 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,21 @@ if st.button('时空传送门'):
 else:
      st.write('Goodbye') 
 
+st.title('Customizing the theme of Streamlit apps')
 
+st.write('Contents of the `.streamlit/config.toml` file of this app')
+
+st.code("""
+[theme]
+primaryColor="#F39C12"
+backgroundColor="#2E86C1"
+secondaryBackgroundColor="#AED6F1"
+textColor="#FFFFFF"
+font="monospace"
+""")
+
+number = st.sidebar.slider('Select a number:', 0, 10, 5)
+st.write('Selected number from slider widget is:', number)
 
 
 
