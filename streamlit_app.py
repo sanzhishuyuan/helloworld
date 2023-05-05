c530db0a9011751ba1d8401bb1a551f09b078ec8
sanzhishuyuan <131636493+sanzhishuyuan@users.noreply.github.com>
2023-05-05 21:54:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import numpy as np
+import pandas as pd
+
 st.header('文件名：streamlit_app.py')
 st.header('30天学会Streamlit')
 
@@ -9,6 +11,31 @@ if st.button('时空传送门'):
      st.markdown(f'<a href="{url}" target="_blank">{url}</a>', unsafe_allow_html=True)
 else:
      st.write('Goodbye')  
+     
+st.write('启动命令：streamlit run streamlit_app.py')
+st.header('30天学会streamlit')
+url = 'https://30days.streamlit.app/?challenge=Day1'
+                
+
+st.header('布局应用')
+
+# 使用for循环生成数字序列
+
+# 创建两列
+col1, col2,col3 = st.columns(3)
+
+# 在第一列中放置一个HTML标签，并应用CSS样式
+with col1:
+    st.write("<div style='height: 200px; width: 100%; background-color: Lime;'>Column 1</div>", unsafe_allow_html=True)
+
+# 在第二列中放置一个HTML标签，并应用CSS样式
+with col2:
+    st.write("<div style='height: 400px; width: 100%; background-color: blue;'>Column 2</div>", unsafe_allow_html=True)
+# 在第三列中放置一个HTML标签，并应用CSS样式
+with col3:
+    st.write("<div style='height: 400px; width: 100%; background-color: green;'> st.write('Aqua - 青色','Black - 黑色' , 'Lime - 酸橙色', 'Olive - 橄榄色','Silver - 银色','White - 白色','Teal - 蓝绿色')</div>", unsafe_allow_html=True)
+
+
      
 st.title('Customizing the theme of Streamlit apps')
 
